f37d276fd6cd8b35e9e115b43a313c5977019a0e
Wesley <matthew282@msn.com>
2022-11-10 21:52:31 -0700

    refactor

--- js_katas.js
+++ js_katas.js
@@ -7,11 +7,18 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 
-function getCount(str) {
-  const count = str.match(/[aeiou]/gi);
-  if (count === null) {
-    return 0
-  } else {
-    return count.length;
-  }  
+// function getCount(str) {
+//   const count = str.match(/[aeiou]/gi);
+//   if (count === null) {
+//     return 0
+//   } else {
+//     return count.length;
+//   }  
+// }
+
+
+//refactor
+
+function getCount(str){
+  return (str.match(/[aeiou]/ig)||[]).length;
 }
